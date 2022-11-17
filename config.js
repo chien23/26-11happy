b7c16930433228795d6224d2cf3f56ecddaf804e
chien23 <118019128+chien23@users.noreply.github.com>
2022-11-17 12:08:04 +0700

    Add files via upload
    create card

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Em';
+const giftUrl = 'https://www.facebook.com/pvchien.dz';
+const eventName = 'Sinh Nhật Em';
+const titleCard = 'Tặng người ấy';
+const contentCard = 'Chúc em tuổi mới tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+
+
+const giftImage = '177883458_816762969188839_6035652318192775762_n.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
